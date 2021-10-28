e8f6293e70ded699941c44df661a07c0450a242a
Jie Jian (JJ) <61826920+pearly0226@users.noreply.github.com>
2021-10-28 13:35:00 -0400

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -92,10 +92,10 @@ const artwork = document.getElementById("artwork")
 // event.offsetX, event.offsetY gives the (x,y) offset from the edge of the canvas.
 
 // Add the event listeners for mousedown, mousemove, and mouseup
-mobile.addEventListener('mousedown', e => {
+mobile.addEventListener('touchstart', e => {
   artwork.setAttribute("style", "display: none;")
 });
-mobile.addEventListener('mouseup', e => {
+mobile.addEventListener('touchend', e => {
   artwork.setAttribute("style", "display: inline-block;")
 });
 
